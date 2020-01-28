4afb1c741a1abc500519adf3e894a92991394a12
Andrew Liakh <liakhandrii@gmail.com>
2020-01-28 16:26:43 +0100

    Initial Commit

---
+++ GKAppStoreConnectApi/GKAppStoreConnectApi.h
@@ -0,0 +1,19 @@
+//
+//  GKAppStoreConnectApi.h
+//  GKAppStoreConnectApi
+//
+//  Created by Andrew Liakh on 28.01.20.
+//  Copyright © 2020 Gikken. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for GKAppStoreConnectApi.
+FOUNDATION_EXPORT double GKAppStoreConnectApiVersionNumber;
+
+//! Project version string for GKAppStoreConnectApi.
+FOUNDATION_EXPORT const unsigned char GKAppStoreConnectApiVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <GKAppStoreConnectApi/PublicHeader.h>
+
+

6c57aae2433bccc1a3567b905865885eaca609a5
Andrew Liakh <liakhandrii@gmail.com>
2020-07-28 17:41:35 +0200

    Added a test account

--- GKAppStoreConnectApi/Models.swift
+++ GKAppStoreConnectApi/Models.swift
@@ -14,6 +14,14 @@ public struct ASCApp {
     public var platform: String
     public var iconUrl: String
     public var name: String
+    
+    public init(id: String, sku: String, platform: String, iconUrl: String, name: String) {
+        self.id = id
+        self.sku = sku
+        self.platform = platform
+        self.iconUrl = iconUrl
+        self.name = name
+    }
 }
 
 public struct ASCUser {
@@ -21,12 +29,25 @@ public struct ASCUser {
     public var personId: String
     public var currentTeamId: String
     public var teams: [ASCTeam]
+    
+    public init(email: String, personId: String, currentTeamId: String, teams: [ASCTeam]) {
+        self.email = email
+        self.personId = personId
+        self.currentTeamId = currentTeamId
+        self.teams = teams
+    }
 }
 
 public struct ASCTeam {
     public var name: String
     public var providerId: Int
     public var apps: [ASCApp]
+    
+    public init(name: String, providerId: Int, apps: [ASCApp]) {
+        self.name = name
+        self.providerId = providerId
+        self.apps = apps
+    }
 }
 
 public struct ASCAppPromoCodesInfo {
@@ -34,12 +55,25 @@ public struct ASCAppPromoCodesInfo {
     public var versionId: Int
     public var contractFilename: String
     public var codesLeft: Int
+    
+    public init(version: String, versionId: Int, contractFilename: String, codesLeft: Int) {
+        self.version = version
+        self.versionId = versionId
+        self.contractFilename = contractFilename
+        self.codesLeft = codesLeft
+    }
 }
 
 public struct ASCAppInternalPurchase {
     public var id: String
     public var name: String
     public var codesLeft: Int
+    
+    public init(id: String, name: String, codesLeft: Int) {
+        self.id = id
+        self.name = name
+        self.codesLeft = codesLeft
+    }
 }
 
 public struct ASCPromoCode {
@@ -49,6 +83,15 @@ public struct ASCPromoCode {
     public var requestId: String
     public var platform: String? = nil
     public var version: String? = nil
+    
+    public init(code: String, creationDate: Date, expirationDate: Date, requestId: String, platform: String? = nil, version: String? = nil) {
+        self.code = code
+        self.creationDate = creationDate
+        self.expirationDate = expirationDate
+        self.requestId = requestId
+        self.platform = platform
+        self.version = version
+    }
 }
 
 public struct ASCInAppPurchasePromoCode {
@@ -56,6 +99,13 @@ public struct ASCInAppPurchasePromoCode {
     public var creationDate: Int
     public var expirationDate: Int
     public var requestId: String
+    
+    public init(code: String, creationDate: Int, expirationDate: Int, requestId: String) {
+        self.code = code
+        self.creationDate = creationDate
+        self.expirationDate = expirationDate
+        self.requestId = requestId
+    }
 }
 
 let GK_ERRORDOMAIN_APPSTORECONNECTAPI_LOGIN = "co.gikken.PromoCodes.AppStoreConnectLogin"
